52d4fb3960b81c1e73eaef383b518f1021f2a4f4
Kontantin Alekseev <alexcseev1990@gmail.com>
2024-05-17 13:42:18 +0300

    add minion project

---
+++ minion/src/js/script.js
@@ -0,0 +1,95 @@
+var landingFunctions = {
+	init: function() {
+		this.initLibraris()
+		// this.time()
+	}, 
+
+	initLibraris: function() {
+		
+		$('[href*="#"]').on('click', function (e) {
+			var fixedOffset = 0;
+			$('html, body')
+				.stop()
+				// .animate({ scrollTop: $(this.hash).offset().top + fixedOffset + (cardHeight - windowHeight)}, 1000);
+				.animate({ scrollTop: $(this.hash).offset().top + fixedOffset}, 1000);
+			e.preventDefault();
+		})
+
+		function menu() {
+			$(window).scroll(function() {
+				var $sections = $('.navigation');
+				$sections.each(function(i,el){
+					var top  = $(el).offset().top-300;
+					var bottom = top +$(el).height();
+					var scroll = $(window).scrollTop();
+					var id = $(el).attr('id');
+					if( scroll > top){
+						$('.nav__link').removeClass('active');
+						$('a[href="#'+id+'"]').addClass('active');
+					}
+				})
+			});
+		}
+	
+		menu()
+
+		$('[data-fancybox]').fancybox({
+			loop: true,
+			infobar: false,
+			animationEffect: false,
+			backFocus: false,
+			hash: false,
+		});
+	},
+
+	time: function() {
+		Date.prototype.daysInMonth = function () {
+			return 32 - new Date(this.getFullYear(), this.getMonth(), 32).getDate();
+		};
+		
+		if (!String.prototype.padStart) {
+			String.prototype.padStart = function padStart(targetLength, padString) {
+				targetLength = targetLength >> 0; //truncate if number or convert non-number to 0;
+				padString = String((typeof padString !== 'undefined' ? padString : ' '));
+				if (this.length > targetLength) {
+					return String(this);
+				}
+				else {
+					targetLength = targetLength - this.length;
+					if (targetLength > padString.length) {
+						padString += padString.repeat(targetLength / padString.length); //append to original to ensure we are longer than needed
+					}
+					return padString.slice(0, targetLength) + String(this);
+				}
+			};
+		}
+
+		function getDate(plusDays) {
+			var now = new Date;
+			now.setDate(now.getDate() + plusDays);
+			var dayNum = "";
+			if (now.getDate() < 10) {
+				dayNum = "0"
+			}
+			dayNum += now.getDate();
+			var monthNum = "";
+			if (now.getMonth() + 1 < 10) {
+				monthNum = "0"
+			}
+			monthNum += now.getMonth() + 1;
+			
+			return dayNum + "." + monthNum + "." + now.getFullYear();
+			// return dayNum + "." + monthNum + "." + String(now.getFullYear()).substr(String(now.getFullYear()).length - 2);
+		}
+
+		// $(".date__1").text(getDate(-5));
+    	$(".date").text(getDate(2));
+
+	},
+
+}
+
+$(document).ready(function() {
+	landingFunctions.init();
+});
+
