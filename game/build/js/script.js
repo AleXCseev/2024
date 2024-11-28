e6f91b758b8b8e61b8fe14c847bbcc9c487f353a
Konstantin Alekseev <alexcseev1990@gmail.com>
2024-11-28 17:51:28 +0200

    add game over and exit

--- game/build/js/script.js
+++ game/build/js/script.js
@@ -1,10 +1,13 @@
 var landingFunctions = {
 	init: function() {
-		this.initLibraries();
+		this.carousel();
 		this.animations();
+		this.modals();
+		this.play();
+		this.range();
 	}, 
 
-	initLibraries: function() {
+	carousel: function() {
 		
 		const owl = $('.modal__avatar-slider').owlCarousel({
 			items: 1,
@@ -38,36 +41,80 @@ var landingFunctions = {
 
 			$(".modal__wrapper-shadow").removeClass().addClass("modal__wrapper-shadow")
 			$(".modal__wrapper-shadow").hide().addClass(color).fadeIn(300)
-		})
+		})	
+		
+		$(document).on("keydown", function(e) {
+			if($("#profile").hasClass("active") || $("#shop").hasClass("active")) {
+				if(e.keyCode == 37) {
+					owl.trigger('prev.owl.carousel');
+				};
+				if(e.keyCode == 39) {
+					owl.trigger('next.owl.carousel');
+				};
+			}
+		});
+	},
+
+	animations: function() {
+		function moveRandomlyWithTransform(element) {
+			const $element = $(element);
+		
+			const distance = Math.floor(Math.random() * 51) + 100;
+		
+			const angle = Math.random() * 360;
+		
+			const deltaX = Math.cos(angle * Math.PI / 180) * distance;
+			const deltaY = Math.sin(angle * Math.PI / 180) * distance;
+		
+			$element.css({
+				transform: `translate(${deltaX}px, ${deltaY}px)`
+			});
+		}
+
+		setInterval(function() {
+			$(".bg__light").each(function() {
+				moveRandomlyWithTransform($(this));
+			})
+		}, 2000);
+	},
+
+	modals: function() {
+
+		const timer = this.timer
 
 		$(".modal-open-triggle").click(function() {
 			$(".modal__wrapper-shadow").hide()
 			$(".main__page").hide();
-			$(".main__page").addClass("hide");
+			$(".main").addClass("hide");
 
 			const id = $(this).data("modal");
 			$(`#${id}`).addClass("active");	
 			$(".modal__wrapper-shadow").removeClass().addClass("modal__wrapper-shadow main")
 			$(".modal__wrapper-shadow").delay(500).fadeIn(1000)
+
+
+			if(id == "exit") {
+				timer();
+			}
 		})	
 
 		$(".btn__back").click(function() {
 			$(".main__page").show();
-			$(".main__page").removeClass("hide");
+			$(".main").removeClass("hide");
 			
 			$(this).closest(".modal__section").removeClass("active");
 		})
 
-		function range() {
-			const value = $(this).val();
-			$(this).closest(".settings__range").find(".range__track").css("width", value + "%");
-			$(this).closest(".settings__range").find(".range__value").text(value + "%");
-		}
+		document.addEventListener("keydown", function(e) {
+			if(e.key === "Escape" || e.code === "Escape") {
+				$(".modal__section").removeClass("active");
+				$(".main__page").show();
+				$(".main").removeClass("hide");
+			}
+		})
+	},
 
-		$(".input__range").on("input", range);
-
-		$(".input__range").each(range);
-
+	play: function() {
 		$(".btn__mode").hover(
 			function() {
 				const mode = $(this).data("mode");
@@ -83,8 +130,6 @@ var landingFunctions = {
 
 		$(".btn__mode").click(function() {
 			const mode = $(this).data("mode");
-			// $(".play__info-item").hide().removeClass("active");
-			// $(`.play__info-item[data-mode="${mode}"]`).fadeIn(300).addClass("active");
 			$(`.play__info-item[data-mode="${mode}"]`).find(".play__info-text").hide()
 			$(`.play__info-item[data-mode="${mode}"]`).find(".play__info-btns").fadeIn(300)
 		})
@@ -95,40 +140,47 @@ var landingFunctions = {
 		})
 	},
 
-	animations: function() {
-		function moveRandomlyWithTransform(element) {
-			const $element = $(element);
-		
-			const distance = Math.floor(Math.random() * 51) + 100;
-		
-			const angle = Math.random() * 360;
-		
-			const deltaX = Math.cos(angle * Math.PI / 180) * distance;
-			const deltaY = Math.sin(angle * Math.PI / 180) * distance;
-		
-			// var transform = $element.css("transform");
-		
-			// var matrix = transform === "none" ? [0, 0, 0, 0, 0, 0] : transform.match(/matrix.*\((.+)\)/)[1].split(', ').map(parseFloat);
-		
-			// var currentX = matrix[4] || 0;
-			// var currentY = matrix[5] || 0;
-
-			// var currentX = 0;
-			// var currentY = 0;
-		
-			// var newX = currentX + deltaX;
-			// var newY = currentY + deltaY;
-		
-			$element.css({
-				transform: `translate(${deltaX}px, ${deltaY}px)`
-			});
+	range: function() {
+		function rangeCallback() {
+			const value = $(this).val();
+			$(this).closest(".settings__range").find(".range__track").css("width", value + "%");
+			$(this).closest(".settings__range").find(".range__value").text(value + "%");
 		}
 
-		setInterval(function() {
-			$(".bg__light").each(function() {
-				moveRandomlyWithTransform($(this));
-			})
-		}, 2000);
+		$(".input__range").on("input", rangeCallback);
+
+		$(".input__range").each(rangeCallback);
+	},
+
+	timer: function() {
+
+		$(".exit__timer-block").show();
+		$(".exit__timer").text(10);
+		$(".exit__block-btns").removeClass("active");
+
+		const time = $(".exit__timer");
+		const intervalId = setInterval(timerDecrement, 1000);
+	  
+		function timerDecrement() {
+			const newTime = time.text() - 1;
+		
+			time.text(newTime);
+		
+			if(newTime === 0) {
+
+				$(".exit__timer-block").hide();
+				$(".exit__block-btns").addClass("active");
+
+				clearInterval(intervalId);
+			} 
+
+		}
+
+		document.addEventListener("keydown", function(e) {
+			if(e.key === "Escape" || e.code === "Escape") {
+				clearInterval(intervalId);
+			}
+		})
 	}
 }
 
