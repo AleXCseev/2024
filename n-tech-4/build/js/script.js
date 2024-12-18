ba7a0a07ff36da8d86abf1579ad269c4cd6eab1b
Konstantin Alekseev <alexcseev1990@gmail.com>
2024-12-18 17:10:53 +0200

    fix

--- n-tech-4/build/js/script.js
+++ n-tech-4/build/js/script.js
@@ -2,23 +2,30 @@ var landingFunctions = {
 	init: function() {
 		this.initLibraries()
 		this.time()
-		// this.video()
 		this.bar()
-		// this.card()
 	}, 
 
 	initLibraries: function() {
 		
 		$('[href*="#"]').on('click', function (e) {
 			var fixedOffset = 0;
-			// var cardHeight = $("#card").outerHeight(false)
-			// var windowHeight = $(window).height()
 
-			$('html, body')
-				.stop()
-				// .animate({ scrollTop: $(this.hash).offset().top + fixedOffset + (cardHeight - windowHeight)}, 1000);
-				.animate({ scrollTop: $(this.hash).offset().top + fixedOffset}, 1000);
-			e.preventDefault();
+			if($(window).width() <= 1080) {
+				var cardHeight = $(".card").outerHeight(false)
+				var windowHeight = $(window).height()
+
+				$('html, body')
+					.stop()
+					.animate({ scrollTop: $(this.hash).offset().top + fixedOffset + (cardHeight - windowHeight)}, 1000);
+					// .animate({ scrollTop: $(this.hash).offset().top + fixedOffset}, 1000);
+				e.preventDefault();
+			} else {
+				$('html, body')
+					.stop()
+					// .animate({ scrollTop: $(this.hash).offset().top + fixedOffset + (cardHeight - windowHeight)}, 1000);
+					.animate({ scrollTop: $(this.hash).offset().top + fixedOffset}, 1000);
+				e.preventDefault();
+			}
 		})
 
 		$(".gallery__slider").owlCarousel({
@@ -29,34 +36,69 @@ var landingFunctions = {
 			items: 5,
 			margin: 10,
 			autoWidth:true,
-			// responsive:{
-			// 	0: {
-			// 		items: 1,
-			// 		center: false,
-			// 		margin: 20,
-			// 	},
-			// 	540: {
-			// 		items: 2,
-			// 		center: false,
-			// 		margin: 20,
-			// 	},
-			// 	1080: {
-			// 		items: 3,
-			// 		center: true,
-			// 		margin: 50,
-			// 	},
-			// 	1280: {
-			// 		items: 3,
-			// 		center: true,
-			// 		margin: 60,
-			// 	},
-			// 	1480: {
-			// 		items: 3,
-			// 		center: true,
-			// 		margin: 80,
-			// 	},
-			// }
+			autoHeight: false,
+			responsive:{
+				0: {
+					items: 1,
+					autoWidth: false,
+					autoHeight: true,
+				},
+				540: {
+					items: 2,
+					autoWidth: false,
+					autoHeight: false,
+				},
+				1080: {
+					items: 4,
+					autoWidth: true,
+					autoHeight: false,
+				},
+				1480: {
+					items: 5,
+					autoWidth: true,
+					autoHeight: false,
+				},
+			}
 		});
+
+		function initialize(){
+			if($(window).width() <= 700) {
+			  	$(".card__photos").addClass("owl-carousel").owlCarousel({
+					items: 4,
+					margin: 10,
+					dots: true,
+					dotsEach: true,
+					nav: false,
+					loop: true,
+				});
+			} else {
+				$(".card__photos").removeClass("owl-carousel").owlCarousel('destroy');
+			}
+			if($(window).width() <= 1080) {
+				$(".review__slider").addClass("owl-carousel").owlCarousel({
+				  items: 1,
+				  margin: 60,
+				  dots: true,
+				  dotsEach: true,
+				  nav: false,
+				  loop: true,
+				  autoHeight: true,
+				  stagePadding: 20,
+			  });
+		  } else {
+			  $(".card__photos").removeClass("owl-carousel").owlCarousel('destroy');
+			  $(".review__slider").removeClass("owl-carousel").owlCarousel('destroy');
+		  }
+		}
+
+		var id;
+
+		$(window).resize( function() {
+			clearTimeout(id);
+			id = setTimeout(initialize, 500);
+		});
+
+		initialize();
 
 		// $(".review__slider").owlCarousel({
 		// 	loop: true,
@@ -88,16 +130,16 @@ var landingFunctions = {
 			}
 		});
 	
-		AOS.init({
-			disable : 'mobile',
-			once: true,
-			duration: 1000,
-			offset : 0,
-		});
+		// AOS.init({
+		// 	disable : 'mobile',
+		// 	once: true,
+		// 	duration: 1000,
+		// 	offset : 0,
+		// });
 	
-		$(window).resize(function() {
-			AOS.refresh();
-		})
+		// $(window).resize(function() {
+		// 	AOS.refresh();
+		// })
 
 		$('[data-fancybox]').fancybox({
 			loop: true,
@@ -131,27 +173,21 @@ var landingFunctions = {
 		}
 
 		function getDate(plusDays) {
-			var today = new Date();
-			var dd = String(today.getDate() + plusDays).padStart(2, '0');
-			var mm = String(today.getMonth() + 1).padStart(2, '0');
-			if (+dd < 0) {
-				mm = String(today.getMonth()).padStart(2, '0');
+			var now = new Date;
+			now.setDate(now.getDate() + plusDays);
+			var dayNum = "";
+			if (now.getDate() < 10) {
+				dayNum = "0"
 			}
+			dayNum += now.getDate();
+			var monthNum = "";
+			if (now.getMonth() + 1 < 10) {
+				monthNum = "0"
+			}
+			monthNum += now.getMonth() + 1;
 			
-			var yyyy = String(today.getFullYear());
-			yyyy = yyyy.substr(yyyy.length - 2);
-			var currentDaysInMonth = new Date().daysInMonth()
-			if (+dd > currentDaysInMonth) {
-				dd = String(dd - currentDaysInMonth).padStart(2, '0');
-				mm = String(+mm + 1).padStart(2, '0');
-			}
-			if (+dd < 0) {
-				dd = String(currentDaysInMonth + +dd).padStart(2, '0');
-			}
-			if (+dd == 0) {
-				dd = "01"
-			}
-			return dd + "." + mm + "." + yyyy
+			// return dayNum + "." + monthNum + "." + now.getFullYear();
+			return dayNum + "." + monthNum + "." + String(now.getFullYear()).substr(String(now.getFullYear()).length - 2);
 		}
 
 		// $(".date__1").text(getDate(-5));
@@ -161,43 +197,6 @@ var landingFunctions = {
 		// $(".card__date .date").text(getDate(2))
 		
 		// $(".year").text(new Date().getFullYear())
-	},
-
-
-	video: function() {
-		var isMobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test((window.navigator.userAgent||window.navigator.vendor||window.opera))||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test((window.navigator.userAgent||window.navigator.vendor||window.opera).substr(0,4));
-		if (isMobile) {
-			var tag = document.createElement('script');
-			tag.src = "https://www.youtube.com/iframe_api";
-			var firstScriptTag = document.getElementsByTagName('script')[0];
-			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-		}
-
-		$(".video").click(function() {
-			$(this).addClass("inactive")
-			if ($("iframe", this).length) {
-				return;
-			}
-
-			var videoId = $(this).data("video");
-
-			if (isMobile) {
-				var videoElId = "video-" + Date.now();
-				$(this).append("<div id='"+videoElId+"'></div>");
-
-				var player = new YT.Player(videoElId, {
-					videoId: videoId,
-					events: {
-						onReady: function() {
-							player.playVideo();
-						}
-					}
-				});
-			} else {
-				var videoSrc = "https://www.youtube.com/embed/" + videoId + "?autoplay=1&mute=0";
-				$(this).append("<iframe src=\""+videoSrc+"\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>");
-			}
-		});
 	},
 
 	bar: function() {
@@ -239,6 +238,7 @@ var landingFunctions = {
 			$(".card__section").show()
 			$(".review__section").show()
 			$(".footer__section").show()
+			$('[href="#order"]').attr('href', '#card')
 		}
 
 		$(".order__btn-start").click(function() {
@@ -274,13 +274,14 @@ var landingFunctions = {
 				$(".review__section").fadeIn(1000)
 				$(".footer__section").fadeIn(1000)
 				$(".card__section").fadeIn(1000)
+				$('[href="#order"]').attr('href', '#card')
 
-				// $([document.documentElement, document.body]).animate(
-				// 	{
-				// 		scrollTop: $(".card__section-wrapper").offset().top,
-				// 	},
-				// 	1000
-				// );
+				$([document.documentElement, document.body]).animate(
+					{
+						scrollTop: $(".card__section-wrapper").offset().top,
+					},
+					1000
+				);
 
 			}, 3000)
 		}
